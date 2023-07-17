82516c35ae9125135c2416020f42ed2beb9cfbc0
Kazuma Yokoe <kazuma.yokoe@nextbeat.net>
2023-07-18 08:57:50 +0900

    Add cats-effect

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,24 @@
+import scala.util.Try
+
 object Main extends App {
-  println("Hello, World!")
+
+  def parseDouble(s: String): Either[String, Double] = {
+    Try(s.toDouble).map(Right(_)).getOrElse(Left(s"$s is not a number"))
+  }
+
+  def divide(a: Double, b: Double): Either[String, Double] = {
+    Either.cond(b != 0, a / b, "Cannot divide by zero")
+  }
+
+  def divisionProgram(inputA: String, inputB: String): Either[String, Double] = {
+    for {
+      a <- parseDouble(inputA)
+      b <- parseDouble(inputB)
+      result <- divide(a, b)
+    } yield result
+  }
+
+  println(divisionProgram("4", "2"))
+  println(divisionProgram("4", "0"))
+
 }
