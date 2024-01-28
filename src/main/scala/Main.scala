842f05c7807d4ac33d15702163d49e1e83639b16
Kazuma Yokoe <kazuma.yokoe@nextbeat.net>
2024-01-28 15:33:20 +0900

    update main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,10 @@
+import typeclasses.validated.ValidatedSample
+
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
 object Main extends App {
 
-  val futureResult1 = EitherSample.divisionProgramAsync("4", "2").value
-
-  val result1 = Await.result(futureResult1, Duration.Inf)
-
-  println(result1)
+  ValidatedSample.executeWithEither()
 
 }
