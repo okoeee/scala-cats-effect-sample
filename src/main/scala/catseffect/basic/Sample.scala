a2469d44b80d90c0d118ecc03638a4c5db736cf2
okoeee <yk27.puxcm@gmail.com>
2024-03-24 22:39:28 +0900

    update

--- src/main/scala/catseffect/basic/Sample.scala
+++ src/main/scala/catseffect/basic/Sample.scala
@@ -2,8 +2,19 @@ package catseffect.basic
 
 import cats.effect.{IO, IOApp}
 
-object Sample extends IOApp.Simple {
+object Sample extends App {
 
   val run = IO.println("Hello, World!")
+
+  val sampleIO1 = IO { println("sample1") }
+  val sampleIO2 = IO { println("sample2") }
+
+  val sampleExecution = for {
+    _ <- sampleIO1
+    _ <- sampleIO2
+  } yield ()
+
+  import cats.effect.unsafe.implicits.global
+  sampleExecution.unsafeRunSync()
 
 }
